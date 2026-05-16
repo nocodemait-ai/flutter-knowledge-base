8283d7e261a1f1479c4be6c12b710c148ccfe4be
Krish-Mistry-04 <krishmistry04@gmail.com>
2026-05-16 17:53:27 +0530

    Added more apps

---
+++ apps/shopping_cart2/README.md
@@ -0,0 +1,5 @@
+# Credits
+
+Model images are from unsplash
+1 . [model1](https://unsplash.com/photos/xrvWzRVwAmM)
+2 . [model2](https://unsplash.com/photos/iun3EnrZKxI)
---
+++ apps/shopping_cart2/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './home_page.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
